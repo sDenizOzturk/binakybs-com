1098c52d0f113648245747c8779661824d8b3f98
S. Deniz Öztürk <serhatdenizozturk@hotmail.com>
2023-11-22 20:16:24 +0300

    convert images to webp

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <Header/>
-  <router-view/>
-  <Footer/>
+  <Header />
+  <router-view />
+  <Footer />
 </template>
 
 <script>
-import Header from '@/components/Header.vue';
-import Footer from '@/components/Footer.vue';
+// eslint-disable-next-line
+import Header from "@/components/Header.vue";
+// eslint-disable-next-line
+import Footer from "@/components/Footer.vue";
 
 export default {
   components: {
@@ -17,18 +19,19 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500&display=swap");
 :root {
-  --font-family: 'Oswald', sans-serif;
+  --font-family: "Oswald", sans-serif;
   --color1: #fbf8f8;
-  --color2: rgb(11,52,4);
-  --color3: rgb(218,219,158);
-  --color3_2: rgba(218,219,158,0.2);
+  --color2: rgb(11, 52, 4);
+  --color3: rgb(218, 219, 158);
+  --color3_2: rgba(218, 219, 158, 0.2);
 }
-html,body {
+html,
+body {
   font-family: var(--font-family);
   background: var(--color1);
-  margin:0;
-  padding:0;
+  margin: 0;
+  padding: 0;
 }
 </style>
